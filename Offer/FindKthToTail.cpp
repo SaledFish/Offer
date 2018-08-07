25a32662bbcd40553ae8dea6fc8711223c45dbed
SaledFish <2289807686@qq.com>
2018-08-07 21:03:08 +0800

    Lines starting # with '#' will be ignored, and an empty message aborts the commit.

--- Offer/FindKthToTail.cpp
+++ Offer/FindKthToTail.cpp
@@ -28,3 +28,30 @@ ListNode *FindKthToTail(ListNode *pListHead, unsigned int k)
 
 	return pBhead;
 }
+
+ListNode *FindKthToTail(ListNode *pListHead, unsigned int k)
+{
+	if (pListHead == nullptr || k == 0)
+		return nullptr;
+
+	ListNode *pAhead = pListHead;
+	ListNode *pBhead = nullptr;
+
+	for (unsigned int i = 0; i < k - 1; ++i)
+	{
+		if (pAhead->m_pNext != nullptr)
+			pAhead = pAhead->m_pNext;
+		else
+			return nullptr;
+	}
+
+	pBhead = pListHead;
+
+	while (pAhead->m_pNext != nullptr)
+	{
+		pAhead = pAhead->m_pNext;
+		pBhead = pBhead->m_pNext;
+	}
+
+	return pBhead;
+}
