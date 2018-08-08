f7ce99da8a77cdfeecc7988a005e7be5ee30ead3
SaledFish <2289807686@qq.com>
2018-08-08 15:29:53 +0800

    8/8

---
+++ Offer/31_IsPopOrder.cpp
@@ -0,0 +1,42 @@
+/*
+栈的压入、弹出序列
+*/
+
+#include <stack>
+
+bool IsPopOrder(const int *pPush, const int *pPop, int nLength)
+{
+	bool bPossible = false;
+
+	if (pPush != nullptr&&pPop != nullptr&&nLength > 0)
+	{
+		const int *pNextPush = pPush;
+		const int *pNextPop = pPop;
+
+		std::stack<int> stackData;
+
+		while (pNextPop - pPop < nLength)
+		{
+			while (stackData.empty() || stackData.top() != *pNextPop)
+			{
+				if (pNextPush - pPush == nLength)
+					break;
+
+				stackData.push(*pNextPush);
+
+				pNextPush++;
+			}
+
+			if (stackData.top() != *pNextPop)
+				break;
+
+			stackData.pop();
+			pNextPop++;
+		}
+
+		if (stackData.empty() && pNextPop - pPop == nLength)
+		bPossible = true;
+	}
+
+	return  bPossible;
+}
